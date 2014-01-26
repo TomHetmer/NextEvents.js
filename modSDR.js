ef42e15a904e6efb609826c68352622967d54fb5
foo- <spamandbeans@gmail.com>
2014-01-26 09:23:31 -0800

    trying to move the event panel

--- modSDR.js
+++ modSDR.js
@@ -92,7 +92,7 @@ whenAvailable("parseEvents", function(t){
 
 
   // Add UTC time above chatbox
-  jQuery("<DIV id=time>TIME</div>").insertBefore("#chatframe")
+  jQuery("<DIV id=time>TIME</div>").insertBefore("#collapse")
   //jQuery("#chatboxspan").prepend("<DIV id=time>TIME</div>");
   setInterval(function(){jQuery("#time").html(new Date().toUTCString() + " Next Event: " + getNextEvent(events))},1000);
 });
